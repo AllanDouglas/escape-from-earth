b0ab52e9dbb6db2bad211c547e04655414cd5932
Allan Douglas <AllanDouglas@mac-mini-b2b-1387.local>
2017-02-17 17:39:12 -0300

    primeiro comit ;)

---
+++ Assets/Src/Scripts/Behaviours/PlanetBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class PlanetBehaviourScript : MonoBehaviour {
+
+	[SerializeField]
+	private int _hitPoints;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate (Vector3.forward, -1, Space.Self);
+	}
+}
